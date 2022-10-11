25471e8a90734a39cdd29031cb140a5c02fc1b57
Ches613 <zverkovmi@gmail.com>
2022-10-11 15:11:26 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// ====================================================================
+// Программа, из имеющегося массива строк формирует массив из строк, 
+// длинна которых равна либо меньше 3 символа
+// ====================================================================
+
+
+// Чтение данных
+string[] ReadData(string text)
+{
+    Console.WriteLine($"{text}:");
+    return Console.ReadLine().Split(",").Select(p => p.Trim()).ToArray();
+}
+
+
+// Возвращает массив строк длинна которых равна либо меньше charCount
+string[] EqualOrLessChar(string[] array, int charCount)
+{
+    string[] buf = new string[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= charCount)
+        {
+            Array.Resize(ref buf, buf.Length + 1);
+            buf[buf.Length - 1] = array[i];
+        }
+    }
+    return buf;
+}
+
+
+// Печать массива
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+PrintArray(
+    EqualOrLessChar(
+        array: ReadData("Введите элементы массива через запятую"),
+        charCount: 3
+    )
+);
